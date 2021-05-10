a4a3bfcd18db3c5c67eae7137bb032bf6349543c
saydul-kader <saydul.kader@gmail.com>
2021-05-11 00:39:46 +0600

    Updated Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">C_Validator</h1>
+                <h1 className="navbar-logo">QR_Scanner</h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className= 'fas fa-bars' ></i>
                 </div>
